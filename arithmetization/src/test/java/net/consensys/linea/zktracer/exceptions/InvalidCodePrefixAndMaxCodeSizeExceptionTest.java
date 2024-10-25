e7cc606772992834cb312e396ff7d886004c0ec7
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-10-25 12:57:06 +0200

    test(maxCodeSize)

--- arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/InvalidCodePrefixAndMaxCodeSizeExceptionTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/InvalidCodePrefixAndMaxCodeSizeExceptionTest.java
@@ -15,19 +15,19 @@
 package net.consensys.linea.zktracer.exceptions;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static net.consensys.linea.zktracer.module.constants.GlobalConstants.MAX_CODE_SIZE;
 import static net.consensys.linea.zktracer.module.hub.signals.TracedException.INVALID_CODE_PREFIX;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
 import net.consensys.linea.testing.BytecodeCompiler;
-import net.consensys.linea.testing.BytecodeRunner;
 import net.consensys.linea.testing.ToyAccount;
 import net.consensys.linea.testing.ToyExecutionEnvironmentV2;
 import net.consensys.linea.testing.ToyTransaction;
+import net.consensys.linea.zktracer.module.hub.signals.TracedException;
 import net.consensys.linea.zktracer.opcode.OpCode;
 import net.consensys.linea.zktracer.types.AddressUtils;
-import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.crypto.KeyPair;
 import org.hyperledger.besu.crypto.SECP256K1;
 import org.hyperledger.besu.datatypes.Address;
@@ -37,14 +37,24 @@ import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.junit.jupiter.api.Test;
 
-public class InvalidCodePrefixExceptionTest {
+public class InvalidCodePrefixAndMaxCodeSizeExceptionTest {
 
   @Test
-  void invalidCodePrefixExceptionViaDeploymentTest() {
+  void invalidCodePrefixExceptionTest() {
     KeyPair keyPair = new SECP256K1().generateKeyPair();
     Address userAddress = Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
     ToyAccount userAccount =
         ToyAccount.builder().balance(Wei.fromEth(1000)).nonce(1).address(userAddress).build();
+
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
+
+    program
+        .push("3d" + "00".repeat(31)) // bytecode starting with 0xef, which is EIP_3541_MARKER
+        .push(0)
+        .op(OpCode.MSTORE)
+        .push(32)
+        .push(0)
+        .op(OpCode.RETURN);
 
     Transaction tx =
         ToyTransaction.builder()
@@ -52,9 +62,7 @@ public class InvalidCodePrefixExceptionTest {
             .keyPair(keyPair)
             .transactionType(TransactionType.FRONTIER)
             .gasLimit(0xffffffL)
-            .payload(
-                Bytes.fromHexString(
-                    "0xef6000600052")) // bytecode starting with 0xef, which is EIP_3541_MARKER
+            .payload(program.compile())
             .build();
 
     Address deployedAddress = AddressUtils.effectiveToAddress(tx);
@@ -67,30 +75,47 @@ public class InvalidCodePrefixExceptionTest {
 
     toyExecutionEnvironment.run();
 
-    /*
     assertEquals(
         INVALID_CODE_PREFIX,
         toyExecutionEnvironment.getHub().currentTraceSection().commonValues.tracedException());
-    */
   }
 
   @Test
-  void invalidCodePrefixExceptionViaReturnTest() {
+  void maxCodeSizeExceptionTest() {
+    KeyPair keyPair = new SECP256K1().generateKeyPair();
+    Address userAddress = Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
+    ToyAccount userAccount =
+        ToyAccount.builder().balance(Wei.fromEth(1000)).nonce(1).address(userAddress).build();
+
     BytecodeCompiler program = BytecodeCompiler.newProgram();
 
-    program
-        .push("ef" + "00".repeat(31))
-        .push(0)
-        .op(OpCode.MSTORE)
-        .push(1)
-        .push(0)
-        .op(OpCode.RETURN);
+    final int NUMBER_OF_EWORDS = 769;
+    for (int i = 0; i < NUMBER_OF_EWORDS; i++) {
+      program.push("3d".repeat(32)).push(32 * i).op(OpCode.MSTORE);
+    }
+    program.push(MAX_CODE_SIZE + 1).push(0).op(OpCode.RETURN);
 
-    BytecodeRunner bytecodeRunner = BytecodeRunner.of(program.compile());
-    bytecodeRunner.run();
+    Transaction tx =
+        ToyTransaction.builder()
+            .sender(userAccount)
+            .keyPair(keyPair)
+            .transactionType(TransactionType.FRONTIER)
+            .gasLimit(0xffffffL)
+            .payload(program.compile())
+            .build();
+
+    Address deployedAddress = AddressUtils.effectiveToAddress(tx);
+    System.out.println("Deployed address: " + deployedAddress);
+
+    checkArgument(tx.isContractCreation());
+
+    ToyExecutionEnvironmentV2 toyExecutionEnvironment =
+        ToyExecutionEnvironmentV2.builder().accounts(List.of(userAccount)).transaction(tx).build();
+
+    toyExecutionEnvironment.run();
 
     assertEquals(
-        INVALID_CODE_PREFIX,
-        bytecodeRunner.getHub().previousTraceSection(2).commonValues.tracedException());
+        TracedException.MAX_CODE_SIZE_EXCEPTION,
+        toyExecutionEnvironment.getHub().currentTraceSection().commonValues.tracedException());
   }
 }
