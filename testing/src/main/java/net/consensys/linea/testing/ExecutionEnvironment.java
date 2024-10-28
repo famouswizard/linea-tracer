aa514ec7aefbc77bfb79721a6a9f3f7d898c40bd
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2024-10-28 12:56:24 +0100

    restore Update ExecutionEnvironment.java

--- testing/src/main/java/net/consensys/linea/testing/ExecutionEnvironment.java
+++ testing/src/main/java/net/consensys/linea/testing/ExecutionEnvironment.java
@@ -73,6 +73,14 @@ public class ExecutionEnvironment {
     } catch (IOException e) {
       throw new RuntimeException(e);
     } finally {
+      if (traceFilePath != null) {
+        if (System.getenv("PRESERVE_TRACE_FILES") == null) {
+          boolean traceFileDeleted = traceFilePath.toFile().delete();
+          final Path finalTraceFilePath = traceFilePath;
+          logger.ifPresent(
+              log -> log.debug("trace file {} deleted {}", finalTraceFilePath, traceFileDeleted));
+        }
+      }
     }
   }
 
